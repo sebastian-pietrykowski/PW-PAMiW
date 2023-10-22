b8af8c25fcbd22cd04ced65f665f940ca46b2b17
Sebastian Pietrykowski <s_pietrykowski@outlook.com>
2023-10-22 18:46:34 +0200

    App now uses services

--- P04WeatherForecastAPI.Client/App.xaml.cs
+++ P04WeatherForecastAPI.Client/App.xaml.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.DependencyInjection;
+using P04WeatherForecastAPI.Client.Services;
+using P04WeatherForecastAPI.Client.ViewModels;
+using System;
 using System.Windows;
 
 namespace P04WeatherForecastAPI.Client
@@ -13,5 +11,32 @@ namespace P04WeatherForecastAPI.Client
     /// </summary>
     public partial class App : Application
     {
+        IServiceProvider _serviceProvider;
+
+        public App()
+        {
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            _serviceProvider = serviceCollection.BuildServiceProvider();
+
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IAccuWeatherService, AccuWeatherService>();
+            services.AddSingleton<IFavoriteCityService, FavoriteCityService>();
+            services.AddSingleton<MainViewModelV4>();
+            services.AddSingleton<FavoriteCityViewModel>();
+           // services.AddSingleton<BaseViewModel,MainViewModelV3>();
+            services.AddTransient<MainWindow>();
+            services.AddTransient<FavoriteCitiesView>();
+        }
+
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            var mainWindow = _serviceProvider.GetService<MainWindow>();
+            mainWindow?.Show();
+        }
+
     }
 }
--- P04WeatherForecastAPI.Client/MainWindow.xaml.cs
+++ P04WeatherForecastAPI.Client/MainWindow.xaml.cs
@@ -20,150 +20,150 @@ namespace P04WeatherForecastAPI.Client
             accuWeatherService = new AccuWeatherService();
         }
 
-        private async void btnSearch_Click(object sender, RoutedEventArgs e)
-        {
+        // private async void btnSearch_Click(object sender, RoutedEventArgs e)
+        // {
             
-            City[] cities= await accuWeatherService.GetLocations(txtCity.Text);
+        //     City[] cities= await accuWeatherService.GetLocations(txtCity.Text);
 
-            // standardowy sposób dodawania elementów
-            //lbData.Items.Clear();
-            //foreach (var c in cities)
-            //    lbData.Items.Add(c.LocalizedName);
+        //     // standardowy sposób dodawania elementów
+        //     //lbData.Items.Clear();
+        //     //foreach (var c in cities)
+        //     //    lbData.Items.Add(c.LocalizedName);
 
-            // teraz musimy skorzystac z bindowania danych bo chcemy w naszej kontrolce przechowywac takze id miasta 
-            lbData.ItemsSource = cities;
-        }
+        //     // teraz musimy skorzystac z bindowania danych bo chcemy w naszej kontrolce przechowywac takze id miasta 
+        //     lbData.ItemsSource = cities;
+        // }
 
-        private async void lbData_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            City? selectedCity= (City)lbData.SelectedItem;
-            if (selectedCity != null) {
-                    await Task.WhenAll(
-                        SetWeatherAndCityName(selectedCity),
-                        SetNeighbors(selectedCity),
-                        SetGeoPosition(selectedCity),
-                        SetTimeZone(selectedCity),
-                        SetAdministrativeArea(selectedCity),
-                        SetFiveDaysDailyForecast(selectedCity),
-                        SetSixHoursHourlyForecast(selectedCity)
-                    );
-            }
-        }
-        private async Task SetWeatherAndCityName(City selectedCity) {
-            var weather = await accuWeatherService.GetCurrentConditions(selectedCity.Key);
-            lblCityName.Content = selectedCity.LocalizedName;
-            double? tempValue = weather?.Temperature.Metric.Value;
-            lblTemperatureValue.Content = Convert.ToString(tempValue) + " °C";
-        }
+        // private async void lbData_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        // {
+        //     City? selectedCity= (City)lbData.SelectedItem;
+        //     if (selectedCity != null) {
+        //             await Task.WhenAll(
+        //                 SetWeatherAndCityName(selectedCity),
+        //                 SetNeighbors(selectedCity),
+        //                 SetGeoPosition(selectedCity),
+        //                 SetTimeZone(selectedCity),
+        //                 SetAdministrativeArea(selectedCity),
+        //                 SetFiveDaysDailyForecast(selectedCity),
+        //                 SetSixHoursHourlyForecast(selectedCity)
+        //             );
+        //     }
+        // }
+        // private async Task SetWeatherAndCityName(City selectedCity) {
+        //     var weather = await accuWeatherService.GetCurrentConditions(selectedCity.Key);
+        //     lblCityName.Content = selectedCity.LocalizedName;
+        //     double? tempValue = weather?.Temperature.Metric.Value;
+        //     lblTemperatureValue.Content = Convert.ToString(tempValue) + " °C";
+        // }
 
-        private async Task SetNeighbors(City selectedCity) {
-            var neighbors = await accuWeatherService.GetNeighbors(selectedCity.Key);
-            var neighborNames = neighbors.Select(city => city.LocalizedName).ToArray();
-            lblNeighborsValue.Content = string.Join("\n", neighborNames);
-        }
+        // private async Task SetNeighbors(City selectedCity) {
+        //     var neighbors = await accuWeatherService.GetNeighbors(selectedCity.Key);
+        //     var neighborNames = neighbors.Select(city => city.LocalizedName).ToArray();
+        //     lblNeighborsValue.Content = string.Join("\n", neighborNames);
+        // }
 
-        private async Task SetGeoPosition(City selectedCity) {
-            var geoPosition = await accuWeatherService.GetGeoPosition(selectedCity.Key);
-            lblGeoPositionLatitudeValue.Content = Convert.ToString(geoPosition?.Latitude) + " °";
-            lblGeoPositionLongitudeValue.Content = Convert.ToString(geoPosition?.Longitude) + " °";
-        }
+        // private async Task SetGeoPosition(City selectedCity) {
+        //     var geoPosition = await accuWeatherService.GetGeoPosition(selectedCity.Key);
+        //     lblGeoPositionLatitudeValue.Content = Convert.ToString(geoPosition?.Latitude) + " °";
+        //     lblGeoPositionLongitudeValue.Content = Convert.ToString(geoPosition?.Longitude) + " °";
+        // }
 
-        private async Task SetTimeZone(City selectedCity) {
-            var timeZone = await accuWeatherService.GetTimeZone(selectedCity.LocalizedName);
-            lblTimeZoneValue.Content = Convert.ToString(timeZone?.GmtOffset + " " + timeZone?.Name);
-        }
+        // private async Task SetTimeZone(City selectedCity) {
+        //     var timeZone = await accuWeatherService.GetTimeZone(selectedCity.LocalizedName);
+        //     lblTimeZoneValue.Content = Convert.ToString(timeZone?.GmtOffset + " " + timeZone?.Name);
+        // }
 
-        private async Task SetAdministrativeArea(City selectedCity) {
-            var administrativeArea = await accuWeatherService.GetAdministrativeArea(selectedCity.LocalizedName);
-            lblAdministrativeAreaValue.Content = Convert.ToString(administrativeArea?.LocalizedName);
-        }
+        // private async Task SetAdministrativeArea(City selectedCity) {
+        //     var administrativeArea = await accuWeatherService.GetAdministrativeArea(selectedCity.LocalizedName);
+        //     lblAdministrativeAreaValue.Content = Convert.ToString(administrativeArea?.LocalizedName);
+        // }
 
-        private async Task SetFiveDaysDailyForecast(City selectedCity) {
-            var fiveDaysDailyForecastCollection = await accuWeatherService.GetFiveDaysDailyForecast(selectedCity.Key);
-            if (fiveDaysDailyForecastCollection == null)
-                    return;
-            DailyForecast[] dailyForecasts = fiveDaysDailyForecastCollection.DailyForecasts;
+        // private async Task SetFiveDaysDailyForecast(City selectedCity) {
+        //     var fiveDaysDailyForecastCollection = await accuWeatherService.GetFiveDaysDailyForecast(selectedCity.Key);
+        //     if (fiveDaysDailyForecastCollection == null)
+        //             return;
+        //     DailyForecast[] dailyForecasts = fiveDaysDailyForecastCollection.DailyForecasts;
 
-            SetFiveDaysDailyForecastDateLabels(dailyForecasts);
-            SetFiveDaysDailyForecastMinTempLabels(dailyForecasts);
-            SetFiveDaysDailyForecastMaxTempLabels(dailyForecasts);
-        }
+        //     SetFiveDaysDailyForecastDateLabels(dailyForecasts);
+        //     SetFiveDaysDailyForecastMinTempLabels(dailyForecasts);
+        //     SetFiveDaysDailyForecastMaxTempLabels(dailyForecasts);
+        // }
 
-        private void SetFiveDaysDailyForecastDateLabels(DailyForecast[] dailyForecasts) {
-            Label[] dateLabels = {
-                lbl5DaysDailyForecast1stDayDateValue,
-                lbl5DaysDailyForecast2ndDayDateValue,
-                lbl5DaysDailyForecast3rdDayDateValue,
-                lbl5DaysDailyForecast4thDayDateValue,
-                lbl5DaysDailyForecast5thDayDateValue
-            };
-            for (int i = 0; i < 5; i++) {
-                dateLabels[i].Content = Convert.ToString(dailyForecasts[i].Date)[..10];
-            }
-        }
+        // private void SetFiveDaysDailyForecastDateLabels(DailyForecast[] dailyForecasts) {
+        //     Label[] dateLabels = {
+        //         lbl5DaysDailyForecast1stDayDateValue,
+        //         lbl5DaysDailyForecast2ndDayDateValue,
+        //         lbl5DaysDailyForecast3rdDayDateValue,
+        //         lbl5DaysDailyForecast4thDayDateValue,
+        //         lbl5DaysDailyForecast5thDayDateValue
+        //     };
+        //     for (int i = 0; i < 5; i++) {
+        //         dateLabels[i].Content = Convert.ToString(dailyForecasts[i].Date)[..10];
+        //     }
+        // }
 
-        private void SetFiveDaysDailyForecastMinTempLabels(DailyForecast[] dailyForecasts) {
-            Label[] minTempLabels = {
-                lbl5DaysDailyForecast1stDayMinTempValue,
-                lbl5DaysDailyForecast2ndDayMinTempValue,
-                lbl5DaysDailyForecast3rdDayMinTempValue,
-                lbl5DaysDailyForecast4thDayMinTempValue,
-                lbl5DaysDailyForecast5thDayMinTempValue
-            };
-            for (int i = 0; i < 5; i++) {
-                var temperature = dailyForecasts[i].Temperature.Minimum;
-                minTempLabels[i].Content = Convert.ToString(temperature.Value)+ " °" + temperature.Unit;
-            }
-        }
+        // private void SetFiveDaysDailyForecastMinTempLabels(DailyForecast[] dailyForecasts) {
+        //     Label[] minTempLabels = {
+        //         lbl5DaysDailyForecast1stDayMinTempValue,
+        //         lbl5DaysDailyForecast2ndDayMinTempValue,
+        //         lbl5DaysDailyForecast3rdDayMinTempValue,
+        //         lbl5DaysDailyForecast4thDayMinTempValue,
+        //         lbl5DaysDailyForecast5thDayMinTempValue
+        //     };
+        //     for (int i = 0; i < 5; i++) {
+        //         var temperature = dailyForecasts[i].Temperature.Minimum;
+        //         minTempLabels[i].Content = Convert.ToString(temperature.Value)+ " °" + temperature.Unit;
+        //     }
+        // }
 
-        private void SetFiveDaysDailyForecastMaxTempLabels(DailyForecast[] dailyForecasts) {
-            Label[] maxTempLabels = {
-                lbl5DaysDailyForecast1stDayMaxTempValue,
-                lbl5DaysDailyForecast2ndDayMaxTempValue,
-                lbl5DaysDailyForecast3rdDayMaxTempValue,
-                lbl5DaysDailyForecast4thDayMaxTempValue,
-                lbl5DaysDailyForecast5thDayMaxTempValue
-            };
-            for (int i = 0; i < 5; i++) {
-                var temperature = dailyForecasts[i].Temperature.Maximum;
-                maxTempLabels[i].Content = Convert.ToString(temperature.Value) + " °" + temperature.Unit;
-            }
-        }
+        // private void SetFiveDaysDailyForecastMaxTempLabels(DailyForecast[] dailyForecasts) {
+        //     Label[] maxTempLabels = {
+        //         lbl5DaysDailyForecast1stDayMaxTempValue,
+        //         lbl5DaysDailyForecast2ndDayMaxTempValue,
+        //         lbl5DaysDailyForecast3rdDayMaxTempValue,
+        //         lbl5DaysDailyForecast4thDayMaxTempValue,
+        //         lbl5DaysDailyForecast5thDayMaxTempValue
+        //     };
+        //     for (int i = 0; i < 5; i++) {
+        //         var temperature = dailyForecasts[i].Temperature.Maximum;
+        //         maxTempLabels[i].Content = Convert.ToString(temperature.Value) + " °" + temperature.Unit;
+        //     }
+        // }
 
-        private async Task SetSixHoursHourlyForecast(City selectedCity) {
-            HourlyForecast[] hourlyForecasts = await accuWeatherService.GetSixHoursHourlyForecast(selectedCity.Key);
+        // private async Task SetSixHoursHourlyForecast(City selectedCity) {
+        //     HourlyForecast[] hourlyForecasts = await accuWeatherService.GetSixHoursHourlyForecast(selectedCity.Key);
 
-            SetSixHoursHourlyForecastHourLabels(hourlyForecasts);
-            SetSixHoursHourlyForecastTempLabels(hourlyForecasts);
-        }
+        //     SetSixHoursHourlyForecastHourLabels(hourlyForecasts);
+        //     SetSixHoursHourlyForecastTempLabels(hourlyForecasts);
+        // }
 
-        private void SetSixHoursHourlyForecastHourLabels(HourlyForecast[] hourlyForecasts) {
-            Label[] dateLabels = {
-                lbl6HoursHourlyForecast1stHourValue,
-                lbl6HoursHourlyForecast2ndHourValue,
-                lbl6HoursHourlyForecast3rdHourValue,
-                lbl6HoursHourlyForecast4thHourValue,
-                lbl6HoursHourlyForecast5thHourValue,
-                lbl6HoursHourlyForecast6thHourValue
-            };
-            for (int i = 0; i < 6; i++) {
-                dateLabels[i].Content = Convert.ToString(hourlyForecasts[i].DateTime).Substring(11, 5);
-            }
-        }
+        // private void SetSixHoursHourlyForecastHourLabels(HourlyForecast[] hourlyForecasts) {
+        //     Label[] dateLabels = {
+        //         lbl6HoursHourlyForecast1stHourValue,
+        //         lbl6HoursHourlyForecast2ndHourValue,
+        //         lbl6HoursHourlyForecast3rdHourValue,
+        //         lbl6HoursHourlyForecast4thHourValue,
+        //         lbl6HoursHourlyForecast5thHourValue,
+        //         lbl6HoursHourlyForecast6thHourValue
+        //     };
+        //     for (int i = 0; i < 6; i++) {
+        //         dateLabels[i].Content = Convert.ToString(hourlyForecasts[i].DateTime).Substring(11, 5);
+        //     }
+        // }
 
-        private void SetSixHoursHourlyForecastTempLabels(HourlyForecast[] hourlyForecasts) {
-            Label[] maxTempLabels = {
-                lbl6HoursHourlyForecast1stHourTempValue,
-                lbl6HoursHourlyForecast2ndHourTempValue,
-                lbl6HoursHourlyForecast3rdHourTempValue,
-                lbl6HoursHourlyForecast4thHourTempValue,
-                lbl6HoursHourlyForecast5thHourTempValue,
-                lbl6HoursHourlyForecast6thHourTempValue
-            };
-            for (int i = 0; i < 6; i++) {
-                var temperature = hourlyForecasts[i].Temperature;
-                maxTempLabels[i].Content = Convert.ToString(temperature.Value) + " °" + temperature.Unit;
-            }
-        }
+        // private void SetSixHoursHourlyForecastTempLabels(HourlyForecast[] hourlyForecasts) {
+        //     Label[] maxTempLabels = {
+        //         lbl6HoursHourlyForecast1stHourTempValue,
+        //         lbl6HoursHourlyForecast2ndHourTempValue,
+        //         lbl6HoursHourlyForecast3rdHourTempValue,
+        //         lbl6HoursHourlyForecast4thHourTempValue,
+        //         lbl6HoursHourlyForecast5thHourTempValue,
+        //         lbl6HoursHourlyForecast6thHourTempValue
+        //     };
+        //     for (int i = 0; i < 6; i++) {
+        //         var temperature = hourlyForecasts[i].Temperature;
+        //         maxTempLabels[i].Content = Convert.ToString(temperature.Value) + " °" + temperature.Unit;
+        //     }
+        // }
     }
 }
