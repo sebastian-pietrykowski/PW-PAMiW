ea646051ac7b7858776d4d491e3560269c0d2517
Sebastian Pietrykowski <s_pietrykowski@outlook.com>
2023-12-29 19:07:50 +0100

    Refactored css code

--- projekt/P06Shop.Shared/Auth/UserLoginDTO.cs
+++ projekt/P06Shop.Shared/Auth/UserLoginDTO.cs
@@ -9,9 +9,12 @@ namespace P06Shop.Shared.Auth
 {
     public class UserLoginDTO
     {
-        [Required]
-        public string Email { get; set; }
-        [Required]
-        public string Password { get; set; }
+        [Required(ErrorMessage="Email is required."),
+        EmailAddress(ErrorMessage="Invalid email format.")]
+        public string Email { get; set; } = string.Empty;
+        
+        [Required(ErrorMessage="Password is required."),
+        MinLength(6, ErrorMessage = "Password must be at least 6 characters long.")]
+        public string Password { get; set; } = string.Empty;
     }
 }
--- projekt/P06Shop.Shared/Auth/UserRegisterDTO.cs
+++ projekt/P06Shop.Shared/Auth/UserRegisterDTO.cs
@@ -10,16 +10,19 @@ namespace P06Shop.Shared.Auth
 
     public class UserRegisterDTO
     {
-        [Required, EmailAddress]
+        [Required(ErrorMessage="Email is required."),
+        EmailAddress(ErrorMessage="Invalid email format.")]
         public string Email { get; set; } = string.Empty;
 
-        [Required, MinLength(6)]
+        [Required(ErrorMessage="Password is required."),
+        MinLength(6, ErrorMessage = "Password must be at least 6 characters long.")]
         public string Password { get; set; } = string.Empty;
 
-        [Compare("Password", ErrorMessage ="The passwords do net match")]
+        [Compare("Password", ErrorMessage ="The passwords do not match.")]
         public string ConfirmPassword { get; set; } = string.Empty;
 
-        [Required, MinLength(3)]
+        [Required(ErrorMessage="Username is required."),
+        MinLength(3, ErrorMessage = "Username must be at least 3 characters long.")]
         public string Username { get; set; } = string.Empty;
     }
 }
